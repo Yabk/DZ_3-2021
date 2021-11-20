aa762882fd2376add971a714ea2afa16d0819df3
Yabk <yabk1010@gmail.com>
2021-11-20 12:52:38 +0100

    Initial commit

---
+++ dz03-rjesenja.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/bash
+
+#!#!#!#!#!#!#
+# 1. Ispisite sve direktorije u /usr/share ciji naziv zapocinje s gtk. (0.5)
+# (potrebno rijesiti koristeci zamjenske znakove)
+
+
+
+#!#!#!#!#!#!#
+# 2. Ispisite sve direktorije u /usr/share koji u nazivu sadrze barem dvije
+# znamenke. (0.5)
+# (potrebno rijesiti koristeci zamjenske znakove)
+
+
+
+#!#!#!#!#!#!#
+# 3. Pronadite rijeci koje sadrze znamenke (ako ih ima) (0.7)
+
+
+
+#!#!#!#!#!#!#
+# 4. Pronadite rijeci koje pocinju slovom i, a u sredini rijeci sadrze veliko
+# slovo. (0.7)
+# Napomena: Radi jednostavnosti, sredina rijeci je definirana kao dio rijeci
+# izmedu prvog i zadnjeg znaka.
+
+
+
+#!#!#!#!#!#!#
+# 5. Pronadite rijeci koje ne sadrze samoglasnike. (ni velike ni male) (0.7).
+# Napomena: Samoglasnici su aeiou. Samoglasnici iz drugih jezika se ne
+# ubrajaju.
+
+
+
+#!#!#!#!#!#!#
+# 6. Pronadite rijeci u kojima se barem dva samoglasnika pojavljuju jedan za
+# drugim. (0.7)
+
+
+
+#!#!#!#!#!#!#
+# 7. Bez koristenja naredbe wc, prebrojite rijeci koje zavrsavaju na ening.
+# (0.7)
+
+
+
+#!#!#!#!#!#!#
+# 8. Bez koristenja naredbe wc, prebrojite rijeci koje zavrsavaju na 's. (0.7)
+
+
+
+#!#!#!#!#!#!#
+# 9. Bez koristenja naredbe wc, prebrojite rijeci koje zavrsavaju na veliko
+# slovo. (0.7)
+
+
+
+#!#!#!#!#!#!#
+# 10. Svim rijecima koje zavrsavaju na 's uklonite znak '. (0.7)
+
+
+
+#!#!#!#!#!#!#
+# 11. Svako pojavljivanje niza word prenesite na pocetak rijeci u kojoj se niz
+# nalazi. (0.7)
+
+
+
+#!#!#!#!#!#!#
+# 12. Svim rijecima koje pocinju na veliko slovo prvo slovo promijenite u malo,
+# a posljednje promijenite u veliko. (0.7)
+# Primjer: Iowa -> iowA
+
+
+
+#!#!#!#!#!#!#
